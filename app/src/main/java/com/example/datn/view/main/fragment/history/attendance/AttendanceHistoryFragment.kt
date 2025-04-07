b038609dfe1aaae8fe89c300d4259a355b141446
lbui2602 <buiducluong2602@gmail.com>
2025-04-07 22:30:31 +0700

    fix

--- app/src/main/java/com/example/datn/view/main/fragment/history/attendance/AttendanceHistoryFragment.kt
+++ app/src/main/java/com/example/datn/view/main/fragment/history/attendance/AttendanceHistoryFragment.kt
@@ -1,10 +1,13 @@
 package com.example.datn.view.main.fragment.history.attendance
 
+import android.app.DatePickerDialog
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -15,10 +18,14 @@ import com.example.datn.click.IClickMess
 import com.example.datn.databinding.FragmentAttendanceBinding
 import com.example.datn.databinding.FragmentAttendanceHistoryBinding
 import com.example.datn.models.attendance.Attendance
+import com.example.datn.models.attendance.GetAttendanceByUserIdRequest
 import com.example.datn.util.SharedPreferencesManager
+import com.example.datn.util.Util
+import com.example.datn.view.main.MainActivity
 import com.example.datn.view.main.adapter.AttendanceAdapter
 import com.example.datn.view.main.fragment.chat.ChatViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.Calendar
 import javax.inject.Inject
 import kotlin.getValue
 
@@ -56,10 +63,28 @@ class AttendanceHistoryFragment : BaseFragment(), IClickAttendance {
             "Bearer "+sharedPreferencesManager.getAuthToken().toString(),
             sharedPreferencesManager.getUserId().toString())
     }
+
     private fun setRecyclerView() {
         binding.rcv.layoutManager = LinearLayoutManager(requireContext())
         adapter = AttendanceAdapter(list,this)
         binding.rcv.adapter = adapter
+    }
+
+    fun showDatePicker(context: Context, onDateSelected: (String) -> Unit) {
+        val calendar = Calendar.getInstance()
+        val datePickerDialog = DatePickerDialog(
+            context,
+            { _, year, month, dayOfMonth ->
+                val dayFormatted = String.format("%02d", dayOfMonth)
+                val monthFormatted = String.format("%02d", month + 1)
+                val formattedDate = "$dayFormatted-$monthFormatted-$year"
+                onDateSelected(formattedDate)
+            },
+            calendar.get(Calendar.YEAR),
+            calendar.get(Calendar.MONTH),
+            calendar.get(Calendar.DAY_OF_MONTH)
+        )
+        datePickerDialog.show()
     }
 
     override fun setView() {
@@ -67,7 +92,14 @@ class AttendanceHistoryFragment : BaseFragment(), IClickAttendance {
     }
 
     override fun setAction() {
-
+        binding.btnCalendar.setOnClickListener {
+            showDatePicker(requireContext(),{
+                val request = GetAttendanceByUserIdRequest(
+                    sharedPreferencesManager.getUserId().toString(), it
+                )
+                viewModel.getAttendanceByUserIdAndDate("Bearer "+sharedPreferencesManager.getAuthToken().toString(),request)
+            })
+        }
     }
 
     override fun setObserves() {
@@ -78,9 +110,16 @@ class AttendanceHistoryFragment : BaseFragment(), IClickAttendance {
                 }
             }
         })
+        viewModel.attendanceByDateResponse.observe(viewLifecycleOwner, Observer { response ->
+            if (response != null && response.code.toInt() == 1) {
+                if(response.attendances !=null){
+                    adapter.updateList(response.attendances.toMutableList())
+                }
+            }
+        })
     }
 
     override fun setTabBar() {
-
+        (requireActivity() as MainActivity).binding.bnvMain.visibility = View.GONE
     }
 }
--- app/src/main/java/com/example/datn/view/main/fragment/history/attendance/AttendanceHistoryViewModel.kt
+++ app/src/main/java/com/example/datn/view/main/fragment/history/attendance/AttendanceHistoryViewModel.kt
@@ -1,10 +1,13 @@
 package com.example.datn.view.main.fragment.history.attendance
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.datn.models.attendance.AttendanceByDateResponse
 import com.example.datn.models.attendance.AttendanceByUserIdResponse
+import com.example.datn.models.attendance.GetAttendanceByUserIdRequest
 import com.example.datn.models.message.MessageResponse
 import com.example.datn.models.password.ChangePasswordRequest
 import com.example.datn.remote.repository.Repository
@@ -23,6 +26,9 @@ class AttendanceHistoryViewModel @Inject constructor(
     private val _attendanceResponse = MutableLiveData<AttendanceByUserIdResponse?>()
     val attendanceResponse: LiveData<AttendanceByUserIdResponse?> get() = _attendanceResponse
 
+    private val _attendanceByDateResponse = MutableLiveData<AttendanceByDateResponse?>()
+    val attendanceByDateResponse: LiveData<AttendanceByDateResponse?> get() = _attendanceByDateResponse
+
     private val _isLoading = MutableLiveData<Boolean?>()
     val isLoading: LiveData<Boolean?> get() = _isLoading
 
@@ -40,6 +46,26 @@ class AttendanceHistoryViewModel @Inject constructor(
 
             } catch (e: Exception) {
                 _attendanceResponse.postValue(null)
+            }
+            _isLoading.postValue(false)
+        }
+    }
+    fun getAttendanceByUserIdAndDate(token:String,request : GetAttendanceByUserIdRequest){
+        Log.e("getAttendanceByUserIdAndDate","hi")
+        viewModelScope.launch {
+            _isLoading.postValue(true)
+            try {
+                val response = repository.getAttendanceByUserIdAndDate(token, request)
+                if(response!=null){
+                    _attendanceByDateResponse.postValue(response)
+                }
+                else{
+                    _attendanceByDateResponse.postValue(null)
+                }
+
+            } catch (e: Exception) {
+                Log.e("getAttendanceByUserIdAndDate",e.toString())
+                _attendanceByDateResponse.postValue(null)
             }
             _isLoading.postValue(false)
         }
